8240030a258224a5b446a8179e7b57e0469c980f
LimDragonIron <ciyciygood@gmail.com>
2025-02-09 10:48:31 +0900

    Modify - logout

--- src/actions/logout.ts
+++ src/actions/logout.ts
@@ -3,6 +3,5 @@
 import { signOut } from "@/auth";
 
 export const logout = async () => {
-  // await signOut({ redirectTo: "/auth/login", redirect: true });
-  await signOut();
+  await signOut({ redirectTo: "/auth/login", redirect: true });
 };
